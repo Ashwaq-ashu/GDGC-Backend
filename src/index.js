0d932e81c63055c458e9b349b465020496512e5b
Peasac <ayaantabrez.10@gmail.com>
2025-10-08 18:01:56 +0530

    Established Auth and mongodb starter files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import express from "express"
 import cors from "cors"
-
+import dotenv from 'dotenv'
+import connectDB from "./mongodb/index.js";
 const app = express();
 
 // Enable JSON body parsing
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+connectDB();
 
-// Prisma Client (generated to ./generated/prisma in schema)
-import { PrismaClient } from "../generated/prisma/index.js";
+
 import { adminRouter } from "./routes/admin.js";
 import { userRouter } from "./routes/user.js";
-const prisma = new PrismaClient();
+
 
 app.get('/',(req , res)=>{
     res.json({
@@ -20,14 +22,9 @@ app.get('/',(req , res)=>{
 })
 
 app.get('/applications', async (req, res) => {
-    try {
-        const applications = await prisma.application.findMany({
-            orderBy: { createdAt: 'desc' }
-        });
-        res.json({ success: true, data: applications });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
+    res.json({
+        message:"simple route for now"
+    })
 })
  
 
