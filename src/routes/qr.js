1160a752dc4b128ceb092d91bc0f3bbd7ab08579
Samiuddin <mdsamiuddin2005@outlook.com>
2025-12-31 17:51:31 +0530

    refactor: improve QR controller and routing structure

--- src/routes/qr.js
+++ src/routes/qr.js
@@ -3,7 +3,9 @@ import { QrController } from "../controllers/QrController.js"
 // import { randomMiddleWare } from "../middleware/AuthMiddleware"
 export const qrRouter = express.Router()
 
-// in this request the persons scan will be redirected to a particular website 
-qrRouter.route('/:id').get(QrController.redirect)
+// More specific routes first
 qrRouter.route('/redirect').post(QrController.updateRedirectionData)
 qrRouter.route('/data').get(QrController.getDataOfQr)
+
+// Dynamic route last (this catches anything that doesn't match above)
+qrRouter.route('/:id').get(QrController.redirect)
