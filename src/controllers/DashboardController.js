f64623f07131b05e99c7319ccef1ce41b9dcf6f7
Peasac <ayaantabrez.10@gmail.com>
2026-01-04 19:33:38 +0530

    get-dashboard returning user info

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,10 +1,11 @@
 import { success } from "zod"
-
+import  User from "../models/User.js"
 export const dashboardController = {
     accessDashboard : async (req,res)=>{
+        const user = await User.findOne({_id:req.id}).select("name email qr_id -_id")
         return res.json({
             success:true,
-            userId:req.id
+            user:user
         })
     }
 }
